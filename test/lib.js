87577b5a5eec0bccf1d94b88b8b35f384fa2c67e
Sébastien Barbieri <sebastien.barbieri@gmail.com>
2017-11-12 21:39:45 +0100

    feat: handle the "we couldn't find that photo issue" (#20)
    * handle the we couldn\'t find that photo issue
    
    * remove useless stateStack from new tests

--- test/lib.js
+++ test/lib.js
@@ -232,3 +232,25 @@ test('download', async t => {
 
   return pify(fs.unlink)(file);
 });
+
+test('download not an image', async t => {
+  const options = {
+    dir: __dirname
+  };
+  const url = 'https://source.unsplash.com/'
+
+  return download(options, url).catch((err)=>{
+    t.is(err.message, 'The response was not an image.');
+  });
+})
+
+test('download error', async t => {
+  const options = {
+    dir: __dirname
+  };
+  const url = 'https://source.unsplash.com/WLUHO9A_xiki/1600x900'
+
+  return download(options, url).catch((err)=>{
+    t.is(err.message, 'The response is the image: we couldn\'t find that photo.');
+  });
+});
